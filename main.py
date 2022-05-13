f51e2fb60e8a335301695815e19775c1ab51519f
Mike Tieden <79123475+miketieden@users.noreply.github.com>
2022-05-12 22:41:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,10 @@
 # Testing
 #
 #
-print("This is a test. This is only a test.")
-print("This is another test.")
+import time
+from datetime import datetime
+f = "%Y-%m-%d %H:%M:%S.%f"
+t = datetime.now()
+timestamp = t.strftime(f)[:-4]
+print(timestamp + " This is a test. This is only a test.")
+print(timestamp + " This is another test.")
